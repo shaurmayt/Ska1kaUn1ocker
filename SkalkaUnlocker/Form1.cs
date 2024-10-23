c650d10a95c0302c8712a444debaf09b85d909b5
Shaurma... <95519502+shaurmayt@users.noreply.github.com>
2024-10-23 14:30:54 +0300

    Add files via upload

---
+++ SkalkaUnlocker/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaskManager;
+
+namespace SkalkaUnlocker
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(diff));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void task_manager_Click(object sender, EventArgs e)
+        {
+            task_manager1 managerForm = new task_manager1();
+            managerForm.Show();
+        }
+
+        private void startup_programs_Click(object sender, EventArgs e)
+        {
+            startup_programs1 startupForm = new startup_programs1();
+            startupForm.Show();
+        }
+
+        private void run_other_tools_Click(object sender, EventArgs e)
+        {
+            //run_other_tools1 startupForm = new run_other_tools1();
+            //startupForm.Show();
+        }
+
+        private void unlock_restrictions_Click(object sender, EventArgs e)
+        {
+            unlock_restrictions startupForm = new unlock_restrictions();
+            startupForm.Show();
+        }
+    }
+}
